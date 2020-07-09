67964b72950739bc391d64920bc411bfd26b1690
nsns0101 <nsns0101@naver.com>
2020-07-09 18:48:30 +0900

    AuthController store

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'destroy']);
+    }
 
     public function index()
     {
